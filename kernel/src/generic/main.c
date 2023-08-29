fa8979dedee85256d98e97c927140536431f3b91
Jacob Garby <j4cobgarby@gmail.com>
2023-08-29 14:05:43 +0100

    testing out framebuffer drawing

--- kernel/src/generic/main.c
+++ kernel/src/generic/main.c
@@ -7,9 +7,28 @@
 #include "fors/init.h"
 #include "fors/memory.h"
 
+volatile struct limine_framebuffer_request framebuf_req = {
+    .id = LIMINE_FRAMEBUFFER_REQUEST,
+    .revision = 0,
+};
+
 void _start(void) {
     arch_early_setup();
     arch_init_memory();
+
+    struct limine_framebuffer *fb = framebuf_req.response->framebuffers[0];
+    uint32_t *fb_arr = fb->address;
+
+    printk("Framebuffer:\n\
+\tAddr: %x\n\
+\tWidth: %d\n\
+\tHeight: %d\n\
+\tPitch: %d\n\
+\tBits/pixel: %d\n", fb->address, fb->width, fb->height, fb->pitch, fb->bpp);
+
+    for (int i = 0; i < 100; i++) {
+        fb_arr[i * (fb->pitch / 4) + i] = 0xffffff;
+    }
     
     for (;;) {
         __asm__("hlt");
